59df9774af4faa9d78800c9463b50deee8300a6d
Jonas Hecht <jonas.hecht@codecentric.de>
2018-10-08 16:05:38 +0200

    vagrant DNS installation should always be made between vagrant up & Ansible execution - so created a create_all.sh

--- create_all.sh
+++ create_all.sh
@@ -1,25 +1,35 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+vagrant up
+
+vagrant dns --install
 
 # Prepare Gitlab on a Server (here Vagrant)
 # If https_internal_server is set to true, be sure to provide providername, providerusername & providertoken (and maybe whitelist your current Internet IP)
 ansible-playbook -i hosts prepare-gitlab.yml --extra-vars "providername=yourProviderNameHere providerusername=yourUserNameHere providertoken=yourProviderTokenHere"
 
 # Only, if you don´t use Vagrant or an only internally accessible Server, you can ignore the extra-vars - Gitlab will handle Let´s Encrypt for you then
-ansible-playbook -i hosts prepare-gitlab.yml
+#ansible-playbook -i hosts prepare-gitlab.yml
 
 ### Provision only certain steps
 
 # Only apt-get update
-ansible-playbook -i hosts prepare-gitlab.yml --tags "update"
+#ansible-playbook -i hosts prepare-gitlab.yml --tags "update"
 
 # Only install Gitlab on the server (skip Docker installation)
-ansible-playbook -i hosts prepare-gitlab.yml --tags "docker"
+#ansible-playbook -i hosts prepare-gitlab.yml --tags "docker"
 
 # Create Let´s Encrypt Certificates for our Vagrant Box (non-publicly accessable server)
-ansible-playbook -i hosts prepare-gitlab.yml --tags "letsencrypt" --extra-vars "providername=yourProviderNameHere providerusername=yourUserNameHere providertoken=yourProviderTokenHere"
+#ansible-playbook -i hosts prepare-gitlab.yml --tags "letsencrypt" --extra-vars "providername=yourProviderNameHere providerusername=yourUserNameHere providertoken=yourProviderTokenHere"
 
 # Install Gitlab only
-ansible-playbook -i hosts prepare-gitlab.yml --tags "install_gitlab"
+#ansible-playbook -i hosts prepare-gitlab.yml --tags "gitlab"
 
 # Install Container Registry only
-ansible-playbook -i hosts prepare-gitlab.yml --tags "configure_registry"
+#ansible-playbook -i hosts prepare-gitlab.yml --tags "registry"
+
+# Install & configure Gitlab Runners only
+#ansible-playbook -i hosts prepare-gitlab.yml --tags "runner"
+
+
